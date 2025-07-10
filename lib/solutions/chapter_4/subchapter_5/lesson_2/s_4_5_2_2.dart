0e25384642ba74d9f0c73522558975a8b68c6e46
Ibo92 <bibomus1@gmail.com>
2025-07-10 16:18:31 +0200

    Task 4522 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -4,6 +4,27 @@ class S4522 extends StatelessWidget {
   const S4522({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyStatelessWidget();
+  }
+}
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          'Hello Ibrahim', // Dein Name hier
+          style: TextStyle(fontSize: 24),
+        ),
+        const SizedBox(height: 20), // Abstand zwischen Text und Button
+        ElevatedButton(
+          onPressed: () {}, // Noch keine Funktion
+          child: const Text('Klick mich'),
+        ),
+      ],
+    );
   }
 }
