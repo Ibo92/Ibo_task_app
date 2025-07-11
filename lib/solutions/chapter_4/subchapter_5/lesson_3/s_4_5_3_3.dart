6f65ce12faa6cdc6843bfebaea60fa15606d3722
Ibo92 <bibomus1@gmail.com>
2025-07-11 12:33:48 +0200

    Task 4533 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,6 +4,30 @@ class S4533 extends StatelessWidget {
   const S4533({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return IboContainerWidget();
+  }
+}
+
+class IboContainerWidget extends StatelessWidget {
+  const IboContainerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 150,
+      color: Colors.blue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('App'),
+          SizedBox(height: 20,),
+          Text('Akademie'),
+        ],
+      ),
+      
+    );
+
   }
 }
