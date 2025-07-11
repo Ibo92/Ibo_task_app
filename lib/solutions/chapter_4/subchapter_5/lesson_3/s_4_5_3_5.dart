3e308c16e35c8ca97e99da89dc1a9a425c2e997a
Ibo92 <bibomus1@gmail.com>
2025-07-11 13:03:57 +0200

    Task4535 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_5.dart
@@ -4,6 +4,60 @@ class S4535 extends StatelessWidget {
   const S4535({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return IboColorWidget();
+  }
+}
+class IboColorWidget extends StatelessWidget {
+  const IboColorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //erste Reihe Row
+        Text('Lösung'),
+        SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color:Colors.red,
+              //Ich habe hier rumgespielt und Abstand zwischen den Container gemacht
+             margin:EdgeInsets.all(5),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color:Colors.green,
+             margin:EdgeInsets.all(5),
+            ),
+          ],
+        ),
+        // Zweite Reihe Row
+        Row(
+mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color:Colors.blue,
+             margin:EdgeInsets.all(5),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color:Colors.yellow,
+             margin:EdgeInsets.all(5), 
+            ),
+          ],
+        ),
+      ],
+    );
+      
+   
+
   }
 }
