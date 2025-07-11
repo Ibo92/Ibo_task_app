5f2a975d0abe56a1a009e58007872b5c1155847a
Ibo92 <bibomus1@gmail.com>
2025-07-11 12:42:20 +0200

    Task4534 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_4.dart
@@ -4,6 +4,26 @@ class S4534 extends StatelessWidget {
   const S4534({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return IboWidget();
+  }
+}
+class IboWidget extends StatelessWidget {
+  const IboWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row( 
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('App'),
+        SizedBox(width:64),
+        Text('Akademie'),
+      ],
+
+    );
+      
+   
+
   }
 }
