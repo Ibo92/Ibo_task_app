539482a49e49e34a7ff9264418354f122ed949b7
Ibo92 <bibomus1@gmail.com>
2025-07-11 12:02:45 +0200

    Task4532 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -4,6 +4,39 @@ class S4532 extends StatelessWidget {
   const S4532({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return IboColumnWidget();
+  }
+}
+class IboColumnWidget extends StatelessWidget {
+  const IboColumnWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 20,),
+       // Erstes Textwidget
+       Text('App Akademie'),
+        SizedBox(height: 20,),
+       // Zweites Textwidget
+       Text('App Akademie', 
+       style: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold ,
+        color: Colors.blue,
+        ),),
+      SizedBox(height: 20,),
+       // Drittes Textwidget
+       Text('App Akademie',
+       style: TextStyle(
+         fontWeight: FontWeight.bold,
+         fontStyle: FontStyle.italic,
+         color: Colors.green,
+       ),
+       ),
+      ],
+    );
+
   }
 }
