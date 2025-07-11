e392d4df99dced69907807abcb6ce755f159ce99
Ibo92 <bibomus1@gmail.com>
2025-07-11 11:48:31 +0200

    Task4531 gelöst

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,43 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ColumnWidget();
+  }
+}
+
+
+class ColumnWidget extends StatelessWidget {
+  const ColumnWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: 20,),
+       // Erster Container (blueGrey)
+       Container(
+        width: 100,
+        height: 100,
+        color: Colors.blueGrey,
+       ),
+       SizedBox(height: 20,),
+        // Zweiter Container (blue)
+       Container(
+        width: 100,
+        height: 100,
+        color: Colors.blue,
+       ),
+       SizedBox(height: 20,),
+        // Dritter Container (Cyan)
+       Container(
+        width: 100,
+        height: 100,
+        color: Colors.cyan,
+       ),
+       SizedBox(height: 20,),
+      ],
+    );
+
   }
 }
